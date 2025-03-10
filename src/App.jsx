8fc74db34000cc6d0f2671f26fff0e3b3d3bfbc2
krit-sh <kritesh1401@gmail.com>
2025-03-10 08:52:35 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import HiddenSearch from "./hiddensearch"
+function App() {
+  return (
+    <>
+      {HiddenSearch()}
+    </>
+  )
+}
+
+export default App
---
+++ src/hiddensearch.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./HiddenSearch.css";
+
+const HiddenSearch = () => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className={`search ${active ? "active" : ""}`}>
+      <input type="text" className="input" placeholder="Search..." />
+      <button className="btn" onClick={() => setActive(!active)}>
+        <i className="fas fa-search"></i>
+      </button>
+    </div>
+  );
+};
+
+export default HiddenSearch;
